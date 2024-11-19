470e1acf73f08b49e806d83bcfd63211959ccb4a
wountv <raiaryan174@gmail.com>
2024-11-19 21:28:26 +0530

    modified:   src/app/components/Home.jsx 	deleted:    src/app/components/NoiseBackground.tsx

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
                 />
             </div>
             <div className="z-10 text-center">
-                <h1 className="text-4xl md:text-6xl font-bold text-blue-600 font-sora mb-4">GDG-USAR</h1>
-                <p className="mt-4 text-base md:text-lg text-[#a92a2a] font-noto-sans">This is a boilerplate component.</p>
+                <h1 className="text-4xl md:text-6xl font-bold text-google-mediumBlue font-sora mb-4">GDG-USAR</h1>
+                <p className="mt-4 text-base md:text-lg text-google-red font-noto-sans">This is a boilerplate component.</p>
             </div>
         </div>
     );
