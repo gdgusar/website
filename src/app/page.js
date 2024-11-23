d583f723063fe37363a30b03bdcaf7b6a73c8e93
Ishu Singh <ishu.111636@yahoo.com>
2024-11-23 23:28:16 +0530

    added footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Hero from './components/hero';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
   <div>
     <Hero/>
+    <Footer/>
   </div>
     
   );
