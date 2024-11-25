4d0cc57108c53f689a9a0b7ea608d7cc8f78960a
wountv <raiaryan174@gmail.com>
2024-11-25 22:55:18 +0530

    new file:   public/assets/illustrations/arrow.svg 	new file:   public/assets/illustrations/audio boombox.svg 	new file:   public/assets/illustrations/chess.svg 	new file:   public/assets/illustrations/gear.svg 	new file:   public/assets/illustrations/knight chess.svg 	new file:   public/assets/illustrations/scales.svg 	renamed:    public/assets/models/gdg_logo1.glb -> public/assets/models/gdg_logo_updated.glb 	new file:   public/assets/models/scene.splinecode 	deleted:    public/assets/svgs/Perspective Grid1.svg 	modified:   src/app/components/Scene.jsx 	new file:   src/app/components/ScrambleText.jsx 	modified:   src/app/components/hero.jsx 	modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,28 +3,32 @@ import React from 'react';
 import Hero from './components/hero';
 import BentoGrid from './components/BentoGrid';
 import WeTextSection from './components/WeTextSection';
+import ScrambleText from './components/ScrambleText'; // Add this line
 
 export default function Home() {
   return (
-    <div className="relative">
-
+    <div className="">
       {/* Main content */}
-      <div className="relative min-h-screen">
+      <div className="">
         <main className="relative z-10">
-          <section className="h-screen">
+          <section className="h-screen w-full">
             <Hero />
           </section>
 
-          <section>
-            <BentoGrid />
+          <section className="container mx-auto px-4 md:px-6 lg:px-8 py-12 md:py-16 lg:py-20">
+            <div className="max-w-7xl mx-auto">
+              <BentoGrid />
+            </div>
           </section>
 
-          <section>
+          <section className="container mx-auto">
             <WeTextSection />
+          </section>
+          <section className="container mx-auto">
+            <ScrambleText />
           </section>
         </main>
       </div>
     </div>
   );
 }
-
