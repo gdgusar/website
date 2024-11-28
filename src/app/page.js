79860263b6be5224af0f2d01b8da2ad771d2d9f5
wountv <raiaryan174@gmail.com>
2024-11-28 23:02:49 +0530

    modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -17,6 +17,9 @@ export default function Home() {
           <section className='h-screen w-full'>
             <Hero/>
           </section>
+          <section>
+            <About/>
+          </section>
 
           <section className="container mx-auto px-4 md:px-6 lg:px-8 py-12 md:py-16 lg:py-20">
             <div className="max-w-7xl mx-auto">
