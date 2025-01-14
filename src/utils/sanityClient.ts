c96aaca47aecb30a6646a37889e6c7bdabe83464
aryanbuilds <raiaryan174@gmail.com>
2025-01-14 13:09:40 +0530

    modified:   .gitignore

--- src/utils/sanityClient.ts
+++ src/utils/sanityClient.ts
@@ -2,8 +2,8 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 
 const client = createClient({
-  projectId: "", // Replace with your actual project ID
-  dataset: "", // Replace with your actual dataset
+  projectId: "enlu62ww", // Replace with your actual project ID
+  dataset: "production", // Replace with your actual dataset
   apiVersion: "2022-03-07",
   useCdn: false,
 });
